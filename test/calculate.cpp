2a44d9f6ac019a1eb3e7955ad2ea0d74cac2ed7f
Seth Bashford <seth.bashford@boystown.org>
2020-11-02 16:36:30 -0600

    refactor

--- test/calculate.cpp
+++ test/calculate.cpp
@@ -1,20 +1,10 @@
 #include "calculate.hpp"
-#include <gsl/gsl>
+#include "usd.hpp"
 #include <sbash64/budget/calculate.hpp>
 
 namespace sbash64 {
 namespace budget {
 namespace calculate {
-constexpr static auto operator"" _cents(unsigned long long cents) -> USD {
-  return USD{gsl::narrow<int_least64_t>(cents)};
-}
-
-static void assertEqual(testcpplite::TestResult &result, USD expected,
-                        USD actual) {
-  assertEqual(result, gsl::narrow<unsigned long long>(expected.cents),
-              gsl::narrow<unsigned long long>(actual.cents));
-}
-
 void differenceHavingNoIncomeNorExpenses(testcpplite::TestResult &result) {
   assertEqual(result, 0_cents, difference(Income{0_cents}, Expenses{}));
 }
--- test/parse.cpp
+++ test/parse.cpp
@@ -1,20 +1,10 @@
 #include "parse.hpp"
-#include <gsl/gsl>
+#include "usd.hpp"
 #include <sbash64/budget/parse.hpp>
 
 namespace sbash64 {
 namespace budget {
 namespace parse {
-constexpr static auto operator"" _cents(unsigned long long cents) -> USD {
-  return USD{gsl::narrow<int_least64_t>(cents)};
-}
-
-static void assertEqual(testcpplite::TestResult &result, USD expected,
-                        USD actual) {
-  assertEqual(result, gsl::narrow<unsigned long long>(expected.cents),
-              gsl::narrow<unsigned long long>(actual.cents));
-}
-
 static void assertEqual(testcpplite::TestResult &result, USD expected,
                         std::string_view s) {
   assertEqual(result, expected, usd(s));
