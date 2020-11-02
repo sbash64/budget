19581d75e4282fccf8c067b8f17a378b6f53304f
Seth Bashford <seth.bashford@boystown.org>
2020-11-02 11:50:01 -0600

    refactor

---
+++ test/parse.hpp
@@ -0,0 +1,14 @@
+#ifndef SBASH64_BUDGET_TEST_PARSE_HPP_
+#define SBASH64_BUDGET_TEST_PARSE_HPP_
+
+#include <sbash64/testcpplite/testcpplite.hpp>
+
+namespace sbash64 {
+namespace budget {
+namespace parse {
+void zero(testcpplite::TestResult &result);
+} // namespace parse
+} // namespace budget
+} // namespace sbash64
+
+#endif
