fab218896e65df4d47a806beff5a0b257e5192c8
Seth Bashford <seth.bashford@boystown.org>
2020-11-03 08:54:24 -0600

    s

--- test/usd.cpp
+++ test/usd.cpp
@@ -6,6 +6,7 @@ namespace budget {
 auto operator"" _cents(unsigned long long cents) -> USD {
   return USD{gsl::narrow<int_least64_t>(cents)};
 }
+
 void assertEqual(testcpplite::TestResult &result, USD expected, USD actual) {
   assertEqual(result, gsl::narrow<unsigned long long>(expected.cents),
               gsl::narrow<unsigned long long>(actual.cents));
