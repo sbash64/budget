621c3a735e6327746bdfa9a4a85ce6819e7139bf
Seth Bashford <seth.bashford@protonmail.com>
2020-12-29 22:03:35 -0600

    same

--- lib/collect.cpp
+++ lib/collect.cpp
@@ -1,29 +1,35 @@
 #include "collect.hpp"
 
 namespace sbash64::budget::collect {
+static auto contains(const ExpenseTree &expenseTree,
+                     const RecursiveExpense &expense) -> bool {
+  return expenseTree.categorizedExpenseTreesOrCosts.count(expense.category) ==
+         0;
+}
+
 static void add(ExpenseTree &expenseTree, const RecursiveExpense &expense) {
   if (std::holds_alternative<USD>(expense.subexpenseOrUsd)) {
-    if (expenseTree.categorizedExpenseTreesOrCosts.count(expense.category) == 0)
+    if (contains(expenseTree, expense))
       expenseTree.categorizedExpenseTreesOrCosts[expense.category] = USD{0};
 
-    expenseTree.categorizedExpenseTreesOrCosts[expense.category] =
+    expenseTree.categorizedExpenseTreesOrCosts.at(expense.category) =
         std::get<USD>(
-            expenseTree.categorizedExpenseTreesOrCosts[expense.category]) +
+            expenseTree.categorizedExpenseTreesOrCosts.at(expense.category)) +
         std::get<USD>(expense.subexpenseOrUsd);
   } else {
-    if (expenseTree.categorizedExpenseTreesOrCosts.count(expense.category) == 0)
+    if (contains(expenseTree, expense))
       expenseTree.categorizedExpenseTreesOrCosts[expense.category] =
           ExpenseTree{};
     add(std::get<ExpenseTree>(
-            expenseTree.categorizedExpenseTreesOrCosts[expense.category]),
+            expenseTree.categorizedExpenseTreesOrCosts.at(expense.category)),
         std::get<Subexpense>(expense.subexpenseOrUsd));
   }
 }
 
-auto expenseTree(const std::vector<RecursiveExpense> &v) -> ExpenseTree {
+auto expenseTree(const std::vector<RecursiveExpense> &expenses) -> ExpenseTree {
   ExpenseTree expenseTree;
-  for (auto x : v)
-    add(expenseTree, x);
+  for (const auto &expense : expenses)
+    add(expenseTree, expense);
   return expenseTree;
 }
 } // namespace sbash64::budget::collect
