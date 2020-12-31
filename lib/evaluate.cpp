1b4b86114f0ea01be11fb8792eb138aab86ff26d
Seth Bashford <seth.bashford@boystown.org>
2020-12-31 12:18:13 -0600

    refactor

--- lib/evaluate.cpp
+++ lib/evaluate.cpp
@@ -4,17 +4,19 @@
 #include <sstream>
 
 namespace sbash64::budget::evaluate {
-static auto getNext(std::stringstream &stream) -> std::string {
+constexpr auto multiWordArgumentDelimiter{'"'};
+
+static auto nextArgument(std::stringstream &stream) -> std::string {
   stream >> std::ws;
-  std::string next;
-  if (stream.peek() == '"') {
+  std::string argument;
+  if (stream.peek() == multiWordArgumentDelimiter) {
     stream.get();
-    getline(stream, next, '"');
+    getline(stream, argument, multiWordArgumentDelimiter);
     stream.get();
   } else {
-    stream >> next;
+    stream >> argument;
   }
-  return next;
+  return argument;
 }
 
 class InvalidCommand {};
@@ -23,15 +25,15 @@ static void initialize(RecursiveExpense &expense, std::stringstream &stream,
                        std::string_view category,
                        std::forward_list<RecursiveExpense> &expenses) {
   expense.category.name = category;
-  auto next{getNext(stream)};
-  if (next.empty())
+  const auto argument{nextArgument(stream)};
+  if (argument.empty())
     throw InvalidCommand{};
-  if (parse::isUsd(next)) {
-    expense.subexpenseOrUsd = parse::usd(next);
+  if (parse::isUsd(argument)) {
+    expense.subexpenseOrUsd = parse::usd(argument);
   } else {
     expenses.push_front({});
     auto &subexpense{expenses.front()};
-    initialize(subexpense, stream, next, expenses);
+    initialize(subexpense, stream, argument, expenses);
     expense.subexpenseOrUsd.emplace<Subexpense>(subexpense);
   }
 }
