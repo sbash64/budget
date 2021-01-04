c4ffaa65e851e0019d40cd147c2f01f1404c81bc
Seth Bashford <seth.bashford@boystown.org>
2021-01-04 13:20:03 -0600

    refactor

--- lib/bank.cpp
+++ lib/bank.cpp
@@ -1,10 +1,18 @@
 #include "bank.hpp"
 
 namespace sbash64::budget {
-Bank::Bank(AccountFactory &factory)
-    : factory{factory}, masterAccount{factory.make("master")} {}
+constexpr auto masterAccountName{"master"};
+constexpr auto transferDescription{"transfer"};
 
-void Bank::credit(const Transaction &t) { masterAccount->credit(t); }
+static void credit(const std::shared_ptr<Account> &account,
+                   const Transaction &t) {
+  account->credit(t);
+}
+
+Bank::Bank(AccountFactory &factory)
+    : factory{factory}, masterAccount{factory.make(masterAccountName)} {}
+
+void Bank::credit(const Transaction &t) { budget::credit(masterAccount, t); }
 
 static void createNewAccountIfNeeded(
     std::map<std::string, std::shared_ptr<Account>> &accounts,
@@ -21,9 +29,14 @@ void Bank::debit(std::string_view accountName, const Transaction &t) {
 
 void Bank::transferTo(std::string_view accountName, USD amount, Date date) {
   createNewAccountIfNeeded(accounts, factory, accountName);
-  masterAccount->debit(
-      Transaction{amount, "transfer to " + std::string{accountName}, date});
-  accounts.at(accountName.data())
-      ->credit(Transaction{amount, "transfer from master", date});
+  masterAccount->debit(Transaction{amount,
+                                   std::string{transferDescription} + " to " +
+                                       std::string{accountName},
+                                   date});
+  budget::credit(accounts.at(accountName.data()),
+                 Transaction{amount,
+                             std::string{transferDescription} + " from " +
+                                 std::string{masterAccountName},
+                             date});
 }
 } // namespace sbash64::budget
