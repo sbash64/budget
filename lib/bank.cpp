540da735f9730b5a78e1d64188b97a54f1052862
Seth Bashford <seth.bashford@boystown.org>
2021-01-04 12:29:50 -0600

    refactor

--- lib/bank.cpp
+++ lib/bank.cpp
@@ -6,17 +6,23 @@ Bank::Bank(AccountFactory &factory)
 
 void Bank::credit(const Transaction &t) { masterAccount->credit(t); }
 
-void Bank::debit(std::string_view accountName, const Transaction &t) {
+static void createNewAccountIfNeeded(
+    std::map<std::string, std::shared_ptr<Account>> &accounts,
+    AccountFactory &factory, std::string_view accountName) {
   if (accounts.count(accountName.data()) == 0)
     accounts[accountName.data()] = factory.make(accountName);
+}
+
+void Bank::debit(std::string_view accountName, const Transaction &t) {
+  createNewAccountIfNeeded(accounts, factory, accountName);
   const auto account{accounts.at(accountName.data())};
   account->debit(t);
 }
 
 void Bank::transferTo(std::string_view accountName, USD amount, Date date) {
-  if (accounts.count(accountName.data()) == 0)
-    accounts[accountName.data()] = factory.make(accountName);
-  masterAccount->debit(Transaction{amount, "transfer to giraffe", date});
+  createNewAccountIfNeeded(accounts, factory, accountName);
+  masterAccount->debit(
+      Transaction{amount, "transfer to " + std::string{accountName}, date});
   accounts.at(accountName.data())
       ->credit(Transaction{amount, "transfer from master", date});
 }
