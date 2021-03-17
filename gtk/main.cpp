ff2f677bf0c366ac4adf1da6272b96dc007f5566
Seth Bashford <seth.bashford@protonmail.com>
2021-03-17 10:14:11 -0500

    another unref

--- gtk/main.cpp
+++ gtk/main.cpp
@@ -389,5 +389,7 @@ int main(int argc, char *argv[]) {
       gtk_application_new("com.sbash64.GtkBudget", G_APPLICATION_FLAGS_NONE)};
   g_signal_connect(app, "activate", G_CALLBACK(sbash64::budget::on_activate),
                    NULL);
-  return g_application_run(G_APPLICATION(app), argc, argv);
+  const auto status{g_application_run(G_APPLICATION(app), argc, argv)};
+  g_object_unref(app);
+  return status;
 }
