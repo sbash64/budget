018d5ac8fc48a1db769b691aba8c6893c7840413
Seth Bashford <seth.bashford@protonmail.com>
2021-07-03 18:56:25 -0500

    refactor

--- web/main.js
+++ web/main.js
@@ -1,18 +1,20 @@
+function createChild(parent, tagName) {
+  const child = document.createElement(tagName);
+  parent.append(child);
+  return child;
+}
+
 function initializeAccounts(accountTable) {
-  const header = document.createElement("tr");
-  const name = document.createElement("th");
+  const header = createChild(accountTable, "tr");
+  const name = createChild(header, "th");
   name.textContent = "Name";
-  header.append(name);
-  const balance = document.createElement("th");
+  const balance = createChild(header, "th");
   balance.textContent = "Balance";
-  header.append(balance);
-  accountTable.append(header);
 }
 
 function main() {
-  const accountTable = document.createElement("table");
+  const accountTable = createChild(document.body, "table");
   initializeAccounts(accountTable);
-  document.body.append(accountTable);
   const accounts = new Map();
   const accountSummaries = new Map();
   const websocket = new WebSocket("ws://localhost:9012");
@@ -21,31 +23,22 @@ function main() {
     const message = JSON.parse(event.data);
     switch (message.method) {
       case "add account": {
-        const accountSummary = document.createElement("tr");
-        const name = document.createElement("td");
-        accountSummary.append(name);
+        const accountSummary = createChild(accountTable, "tr");
+        const name = createChild(accountSummary, "td");
         name.textContent = message.name;
-        const balance = document.createElement("td");
-        accountSummary.append(balance);
-        accountTable.append(accountSummary);
+        createChild(accountSummary, "td");
         accountSummaries.set(message.name, accountSummary);
 
-        const account = document.createElement("table");
-        const header = document.createElement("tr");
-        const description = document.createElement("th");
+        const account = createChild(document.body, "table");
+        const header = createChild(account, "tr");
+        const description = createChild(header, "th");
         description.textContent = "Description";
-        header.append(description);
-        const debits = document.createElement("th");
+        const debits = createChild(header, "th");
         debits.textContent = "Debits";
-        header.append(debits);
-        const credits = document.createElement("th");
+        const credits = createChild(header, "th");
         credits.textContent = "Credits";
-        header.append(credits);
-        const date = document.createElement("th");
+        const date = createChild(header, "th");
         date.textContent = "Date";
-        header.append(date);
-        account.append(header);
-        document.body.append(account);
         accounts.set(message.name, account);
 
         account.style.display = "none";
@@ -62,36 +55,28 @@ function main() {
         break;
       case "add credit": {
         const account = accounts.get(message.name);
-        const transaction = document.createElement("tr");
-        const description = document.createElement("td");
+        const transaction = createChild(account, "tr");
+        const description = createChild(transaction, "td");
         description.textContent = message.description;
-        transaction.append(description);
         transaction.append(document.createElement("td"));
-        const amount = document.createElement("td");
+        const amount = createChild(transaction, "td");
         amount.textContent = message.amount;
-        transaction.append(amount);
-        const date = document.createElement("td");
+        const date = createChild(transaction, "td");
         date.textContent = message.date;
-        transaction.append(date);
-        account.append(transaction);
         break;
       }
       case "remove credit":
         break;
       case "add debit": {
         const account = accounts.get(message.name);
-        const transaction = document.createElement("tr");
-        const description = document.createElement("td");
+        const transaction = createChild(account, "tr");
+        const description = createChild(transaction, "td");
         description.textContent = message.description;
-        transaction.append(description);
-        const amount = document.createElement("td");
+        const amount = createChild(transaction, "td");
         amount.textContent = message.amount;
-        transaction.append(amount);
         transaction.append(document.createElement("td"));
-        const date = document.createElement("td");
+        const date = createChild(transaction, "td");
         date.textContent = message.date;
-        transaction.append(date);
-        account.append(transaction);
         break;
       }
       case "remove debit":
