01a990a51ceb08a2b52d119d6c8564f8df533613
Seth Bashford <seth.bashford@protonmail.com>
2021-07-27 21:20:13 -0500

    same

--- lib/include/sbash64/budget/domain.hpp
+++ lib/include/sbash64/budget/domain.hpp
@@ -177,8 +177,8 @@ public:
   virtual auto balance() -> USD = 0;
   virtual void remove() = 0;
   virtual void clear() = 0;
-  virtual void withdraw(USD) {}
-  virtual void deposit(USD) {}
+  virtual void withdraw(USD) = 0;
+  virtual void deposit(USD) = 0;
 
   class Factory {
   public:
